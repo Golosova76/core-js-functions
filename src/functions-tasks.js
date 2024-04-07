27b340c0252d5ea18cb9a579ec47c45554c5981e
Anna Golosova <golosova76@gmail.com>
2024-04-07 19:08:57 +0300

    feat: solution the tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return 'getCurrentFunctionName';
 }
 
 /**
@@ -32,8 +32,12 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (!func) {
+    return '';
+  }
+  const functionBody = `${func}`;
+  return functionBody;
 }
 
 /**
@@ -50,8 +54,15 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  if (!Array.isArray(funcs)) {
+    return [];
+  }
+  const argsCounts = funcs.map(function argsCounts(func) {
+    return func.length;
+  });
+
+  return argsCounts;
 }
 
 /**
